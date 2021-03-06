d58870b7902029f132590b59be7724dce395810e
leandromend <leandromendesdasilvas@gmail.com>
2021-03-06 14:36:28 -0300

    Fundamentos A ritméticos

---
+++ Desafio5.js
@@ -0,0 +1,10 @@
+// Fundamentos Aritméticos em JavaScript
+// 5 / 5 - Consumo Médio do Automóvel
+
+let X = parseInt(gets());
+let Y = parseFloat(gets());
+let Km = X/Y;
+
+
+
+console.log(Km.toFixed(3) + ' Km/l');
